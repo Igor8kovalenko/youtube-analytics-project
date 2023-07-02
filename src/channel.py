b5503f0d2c8bbe3e0f957020dc57191bc4c1ec1a
Dell-PC <Igorek.kovalenko.2015@mail.ru>
2023-07-02 17:02:13 +0300

    начало великого конца

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,26 @@
-
+import json
+import os
+from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
+    API_KEY: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+        self.__channel_id = channel_id
+        self.title = channel['items'][0]['snippet']['title']
+        self.url = f'https://www.youtube.com/channel/{channel_id}'
+        self.count_subscribers = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
