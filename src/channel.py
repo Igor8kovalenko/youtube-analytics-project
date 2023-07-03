dedfc4595e0910b0232662fc0ecedaf30c348e02
Dell-PC <Igorek.kovalenko.2015@mail.ru>
2023-07-03 20:10:23 +0300

    поправил IPY_KEY

--- src/channel.py
+++ src/channel.py
@@ -4,14 +4,14 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
-    API_KEY: str = os.getenv('YT_API_KEY')
+    API_KEY: str = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
-        self.__channel_id = channel_id
+        self.channel_id = channel_id
         self.title = channel['items'][0]['snippet']['title']
         self.url = f'https://www.youtube.com/channel/{channel_id}'
         self.count_subscribers = channel['items'][0]['statistics']['subscriberCount']
